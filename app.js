2769b9e1b6824443912a8a9402de505b9a69410e
Splash <viniciussamuel153@gmail.com>
2023-02-02 16:34:27 -0300

    commit init
    Site da FJU

---
+++ app.js
@@ -0,0 +1,27 @@
+let time = 5000;
+    currentImageIndex = 0;
+    image = document
+                .querySelectorAll("#slider img");
+    max = image.length;
+
+function nextImage() {
+
+    image[currentImageIndex].classList.remove("selected")
+
+    currentImageIndex++
+
+    if(currentImageIndex >=  max)
+        currentImageIndex = 0
+
+    image[currentImageIndex].classList.add("selected")
+}
+
+function start () {
+    setInterval(() => {
+
+       nextImage()
+
+    }, time)
+}
+
+window.addEventListener("load", start)
